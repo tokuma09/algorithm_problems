8642ec2fecfa17f8c483c3726f5275fc979d7b76
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2021-06-19 09:45:14 +0000

    fix long and replace

--- problems/chapter03/tokuma/007.cpp
+++ problems/chapter03/tokuma/007.cpp
@@ -1,4 +1,3 @@
-// TODO
 #include <iostream>
 #include <string>
 
@@ -17,21 +16,24 @@ int main()
         // 文字列そのものを操作するためコピー
         string S_copy = S;
         long long val = 0;
+
         // 各桁を操作する間なので1だけ引く
         for (long long i = 0; i < (S.length() - 1); ++i)
         {
-
             // 1がでたら
             if (bit & (1 << i))
             {
                 // 1がでたところまでを取り出す
-                int part = stoll(S_copy.substr(0, i + 1));
+                long long part = stoll(S_copy.substr(0, i + 1));
                 // 足し算
                 val += part;
-                // そこまでは足し算を終えているので、0に置き換える
-                S_copy.replace(0, i + 1, "0");
+                for (int ind = 0; ind < (i + 1); ++ind)
+                {
+                    S_copy[ind] = '0';
+                }
             }
         }
+
         // 最後まで1が出なかったらそこまでの文字を足す
         // もしすでにでていても、そこまでの部分は0で置き換えているので問題ない
         val += stoll(S_copy);
