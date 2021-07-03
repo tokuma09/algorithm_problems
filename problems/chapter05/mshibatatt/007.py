eac3fbb13f56a08f1fa0220e44bf5b1914cb26e7
mshibatatt <mshibatatt@gmail.com>
2021-07-03 09:26:51 +0000

    modify 5.7

--- problems/chapter05/mshibatatt/007.py
+++ problems/chapter05/mshibatatt/007.py
@@ -1,8 +1,3 @@
-def chmaxlen(a, b):
-    if len(a) >= len(b):
-        return a
-    else: return b
-
 def main():
     s = input()
     t = input()
@@ -12,18 +7,30 @@ def main():
 
     # dp[i][j] is m, where m is common substring with max length from s[0:i+1] and t[0:j+1]  
     dp = [[None]*len_t for i in range(len_s)]
+    # dp_len[i][j] is len(dp[i][j])
+    dp_len = [[None]*len_t for i in range(len_s)]
 
     for i in range(len_s):
         for j in range(len_t):
             if i == 0 and j == 0:
                 dp[i][j] = s[i] if s[i] == t[j] else ''
+                dp_len[i][j] = 1 if s[i] == t[j] else 0
             elif i == 0:
                 dp[i][j] = s[i] if s[i] == t[j] or dp[i][j-1] == s[i] else ''
+                dp_len[i][j] = 1 if s[i] == t[j] or dp[i][j-1] == s[i] else 0
             elif j == 0:
                 dp[i][j] = t[j] if s[i] == t[j] or dp[i-1][j] == t[j] else ''
+                dp_len[i][j] = 1 if s[i] == t[j] or dp[i-1][j] == t[j] else 0
             else:
-                dp[i][j] = dp[i-1][j-1] + s[i] if s[i] == t[j] \
-                     else chmaxlen(dp[i-1][j], dp[i][j-1])
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i-1][j-1] + s[i]
+                    dp_len[i][j] = dp_len[i-1][j-1] + 1
+                elif dp_len[i-1][j] >= dp_len[i][j-1]:
+                    dp[i][j] = dp[i-1][j]
+                    dp_len[i][j] = dp_len[i-1][j]
+                else:
+                    dp[i][j] = dp[i][j-1]
+                    dp_len[i][j] = dp_len[i][j-1]
     
     print(dp[len_s-1][len_t-1])
 
