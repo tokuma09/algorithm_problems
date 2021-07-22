f2bf7989786defae0f53e46cc9fc449ca75bed18
miiitomi <ytommyrad@gmail.com>
2021-07-23 03:10:37 +0900

    modify chapter8 miiitomi

--- problems/chapter08/miiitomi/007.cpp
+++ problems/chapter08/miiitomi/007.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <unordered_set>
 using namespace std;
 
 int main() {
+    bool ans = false;
     // 入力1行目: N K
     // 入力2行目: a_0 a_1 ... a_{N-1}
     // 入力3行目: b_0 b_1 ... b_{N-1}
@@ -15,14 +15,11 @@ int main() {
         cin >> a;
         A.insert(a);
     }
-    vector<int> b(N);
-    for (int i = 0; i < N; i++) cin >> b[i];
-
     for (int i = 0; i < N; i++) {
-        if (A.count(K - b[i])) {
-            cout << "Yes" << endl;
-            return 0;
-        }
+        int b;
+        cin >> b;
+        if (A.count(K - b)) ans = true;
     }
-    cout << "No" << endl;
+    if (ans) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
