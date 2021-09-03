d08db74d75214e7472cbebefd01016ca9d7d8ab7
mshibatatt <38758355+mshibatatt@users.noreply.github.com>
2021-09-03 11:24:34 +0000

    Deploying to gh-pages from  @ bfdabad925113f8c5b572c42253f4ab75bc43e3e 🚀

--- problems/chapter13/mshibatatt/005.py
+++ problems/chapter13/mshibatatt/005.py
@@ -4,34 +4,30 @@ from collections import deque
 def main():
     N, M = map(int, input().split())
     G = [[] for _ in range(N)]
+    deg = [0] * N
     for _ in range(M):
         a, b = map(int, input().split())
         a -= 1
         b -= 1
-        G[a].append(b)
+        # reverse graph
+        G[b].append(a)
+        deg[a] += 1
 
-    seen = [False]*N
     todo = deque([])
+    for i, e in enumerate(deg):
+        if e == 0:
+            todo.append(i)
 
     output = []
-    while len(output) < N:
-        for i, e in enumerate(seen):
-            if not e:
-                seen[i] = True
+    while len(todo) > 0:
+        node = todo.popleft()
+        output.append(node)
+        for i in G[node]:
+            deg[i] -= 1
+            if deg[i] == 0:
                 todo.append(i)
-                break
-        sub_output = []
-        while len(todo) > 0:
-            node = todo.popleft()
-            sub_output.append(node)
-            for i in G[node]:
-                if seen[i]:
-                    continue
-                seen[i] = True
-                todo.append(i)
-        output = sub_output + output
 
-    print(output)
+    print(output[::-1])
 
 if __name__=='__main__':
     main()        
--- problems/chapter13/mshibatatt/006.py
+++ problems/chapter13/mshibatatt/006.py
@@ -1,15 +1,22 @@
 # find cycle in directed graph
+# https://qiita.com/drken/items/a803d4fc4a727e02f7ba
+import sys
+sys.setrecursionlimit = 1000000
 
-def dfs(G, n, seen, path):
+def dfs(G, n, seen, finished, path, pos):
+    seen[n] = True
     path.append(n)
     for nxt in G[n]:
-        if nxt in path:
-            path.append(nxt)
-            return path
-        if seen[nxt]:
+        if finished[nxt]:
             continue
-        seen[nxt] = True
-        return dfs(G, nxt, seen, path)
+        if seen[nxt] == True and finished[nxt] == False:
+            pos = nxt
+            return pos
+        pos = dfs(G, nxt, seen, finished, path, pos)
+        if pos != -1:
+            return pos
+    finished[n] = True
+    path.pop()
 
 def main():
     N, M = map(int, input().split())
@@ -21,17 +28,29 @@ def main():
         G[a].append(b)
 
     seen = [False]*N
+    finished = [False]*N
     output = 'No'
+    path = []
+    pos = -1
 
     while not all(seen):
         for i, e in enumerate(seen):
             if not e:
                 node = i
                 break
-        seen[node] = True
-        cycle = dfs(G, node, seen, [])
-        if cycle != None:
+        
+        pos = dfs(G, node, seen, finished, path, pos)
+        if pos != -1:
             output = 'Yes'
+            cycle = []
+            top = path[-1]
+            while len(path) > 0:
+                t = path[-1]
+                cycle.append(t)
+                path.pop()
+                if t == pos:
+                    cycle.append(top)
+                    break
             break
 
     if output == 'Yes':
