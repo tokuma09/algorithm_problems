9f62bd57bd44b857669e805f6847e8e4f6ffae61
Masahiro Shibata <mshibatatt@gmail.com>
2021-09-19 11:16:11 +0000

    add visualize code

--- problems/chapter16/mshibatatt/007.py
+++ problems/chapter16/mshibatatt/007.py
@@ -60,20 +60,20 @@ def main():
                 num_blank += 1
                 v = i*c + j
                 # if adjacent in row, cannot both 0
-                if i > 0 and G[i-1][j] == '#':
+                if i+1 < r and G[i+1][j] == '#':
                     num_adjacent += 1
                     newv = v + r*c
                     ff.add_edge(newv, t, 1)
                     ff.add_edge(v, newv, float('inf'))
-                    ff.add_edge(v-c, newv, float('inf'))
+                    ff.add_edge(v+c, newv, float('inf'))
                     
                 # if adjacent in col, cannot both 1
-                if j > 0 and G[i][j-1] == '#':
+                if j+1 < c and G[i][j+1] == '#':
                     num_adjacent += 1
                     newv = v + r*c*2
                     ff.add_edge(s, newv, 1)
                     ff.add_edge(newv, v, float('inf'))
-                    ff.add_edge(newv, v-1, float('inf'))
+                    ff.add_edge(newv, v+1, float('inf'))
     
     output = num_blank - num_adjacent + ff.flow(s, t)
     print(output)
